b043625db45d7f2242c4bb5307edb51a0aaa7a14
Leonard Pauli <leonardpauli@me.com>
2018-04-07 19:30:35 +0200

    nginx.example: using-ssl-letsencrypt WIP

--- terminal/nginx/example/setup/deploy.sh
+++ terminal/nginx/example/setup/deploy.sh
@@ -3,7 +3,7 @@
 echo deploying...
 
 envtoload="SSH_PROD SSH_PROD_PREFIX"
-for a in $envtoload; do export $a="$(cat .env | grep $a= | sed -e "s/.*=//g")"; done
+[ -f .env ] && for a in $envtoload; do export $a="$(cat .env | grep $a= | sed -e "s/.*=//g")"; done
 
 deploy_command='git pull && ./run'
 ssh $SSH_PROD "$SSH_PROD_PREFIX $deploy_command"
--- terminal/nginx/example/setup/ssl.prod.sh
+++ terminal/nginx/example/setup/ssl.prod.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
+# using letsencrypt
+#
+# envtoload="SSL_PROD_STAGEING DOMAINS_PROD SSL_PROD_EMAIL"
+# [ -f .env ] && for a in $envtoload; do export $a="$(cat .env | grep $a= | sed -e "s/.*=//g")"; done
+# or: export SSL_PROD_STAGEING=true; export DOMAINS_PROD='example.com'; export SSL_PROD_EMAIL='admin@example.com'
+#
+# ./<program> renew -q
+# ./<program> create
+lepath="$(pwd)/../data/letsencrypt"
 
-echo "TODO: see ssl.local.sh for now"
+
+letsencrypt () {
+	docker run --rm -it \
+		-v $lepath/data:/etc/letsencrypt \
+		-v $lepath/data2:/var/lib/letsencrypt \
+		-v $lepath/public:/var/www/public \
+		certbot/certbot "$@"
+}
+
+if [ "$1" = "create" ]; then
+	stagingFlag="--staging"; [ "$SSL_PROD_STAGEING" = "false" ] && stagingFlag=""
+	letsencrypt certonly --webroot \
+		--agree-tos --no-eff-email \
+		--webroot-path /var/www/public \
+		--cert-name mycert \
+		--domains $DOMAINS_PROD \
+		$stagingFlag --email $SSL_PROD_EMAIL
+else letsencrypt $@; fi
