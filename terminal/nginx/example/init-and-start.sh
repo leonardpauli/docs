d1f6719df10e4378bb9bc5c28947a9c17f3e111a
Leonard Pauli <leonardpauli@me.com>
2018-04-08 14:19:14 +0200

    nginx.example: WIP ssl.prod (corrected links)

--- terminal/nginx/example/init-and-start.sh
+++ terminal/nginx/example/init-and-start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "INIT and START" \
-&& cp -R setup/makeshift-data/ssl /etc/nginx && cp -R data/ssl /etc/nginx \
+&& mkdir -p /etc/nginx/ssl \
+	&& ln -sf $(pwd)/setup/makeshift-data/ssl/* /etc/nginx/ssl/ \
+	&& ln -sf $(pwd)/data/ssl/* /etc/nginx/ssl/ \
 && (cd /etc/nginx/sites && ./init.sh) \
 && /usr/sbin/nginx -g "daemon off;"
--- terminal/nginx/example/setup/ssl.prod.sh
+++ terminal/nginx/example/setup/ssl.prod.sh
@@ -2,27 +2,29 @@
 # using letsencrypt
 # OBS: not pure
 # ./<program> renew
-# ./<program> create output/cert-and-key-prefix-path "Some Name" "example.com,www.example.com" "true" "admin@example.com"
-lepathrel="../data/letsencrypt"
-lepath="$(pwd)/$lepathrel"
+# ./<program> create output-cert-and-key-prefix-name "Some Name" "example.com,www.example.com" "true" "admin@example.com"
+data_path="$(pwd)/../data"
+lepath="letsencrypt"
+sslpath="ssl"
+ssl_to_data_path=".."
 
 letsencrypt () {
 	docker run --rm -it \
-		-v $lepath/data:/etc/letsencrypt \
-		-v $lepath/public:/var/www/public \
+		-v $data_path/$lepath/data:/etc/letsencrypt \
+		-v $data_path/$lepath/public:/var/www/public \
 		certbot/certbot "$@"
 }
 
 if [ "$1" = "create" ]; then
-	filename=$2; # path/to/file-prefix // $filename.{crt,key}
+	lepath_after_base=
+	filename=$2; # file-prefix // $filename.{crt,key}
 	# name=${3:-"Some Name"} # show to user
 	domains=${4:-"localhost,my-app.localhost,local.my-app.com"}
 	use_staging=${5:-"true"}
 	email=$6
 	
-	# TODO: would $lepath work just as well?
-	ln -sf $lepathrel/live/mycert/fullchain.pem $filename.crt
-	ln -sf $lepathrel/live/mycert/privkey.pem $filename.key
+	ln -sf $ssl_to_data_path/$lepath/data/live/mycert/fullchain.pem $data_path/$sslpath/$filename.crt
+	ln -sf $ssl_to_data_path/$lepath/data/live/mycert/privkey.pem $data_path/$sslpath/$filename.key
 
 	stagingFlag="--staging"; [ "$use_staging" = "false" ] && stagingFlag=""
 	letsencrypt certonly --webroot \
