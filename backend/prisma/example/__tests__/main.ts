31a4543ff0fbf160c3a18ace6c8254fa0ba0525b
Leonard Pauli <leonardpauli@me.com>
2018-10-03 23:18:58 +0200

    backend.prisma: example builds + runs + accesses db

--- backend/prisma/example/__tests__/main.ts
+++ backend/prisma/example/__tests__/main.ts
@@ -1,6 +1,9 @@
 describe('example', ()=> {
-  const name: string = 'Anna';
-  it('works', () => {
-    expect('Hello, Anna').toBe(`Hello, ${name}`);
-  });
+	const name: string = 'Anna';
+	it('works', () => {
+		expect('Hello, Anna').toBe(`Hello, ${name}`);
+	});
+	it('math', () => {
+		expect(1 + 1).toBe(2)
+	})
 });
--- backend/prisma/example/src/main.ts
+++ backend/prisma/example/src/main.ts
@@ -1,3 +1,12 @@
-const msg: string = 'yes'
+import {prisma} from './generated/prisma'
 
-console.log(`works: ${msg}`)
+async function main() {
+	const newUser = await prisma.createUser({name: 'Anna'})
+	console.log(`created user ${newUser.name} (${newUser.id})`)
+
+	const allUsers = await prisma.users()
+	console.log(allUsers)
+
+}
+
+main().catch(console.error)
