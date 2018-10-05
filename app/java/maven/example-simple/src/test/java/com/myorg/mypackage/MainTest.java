724fbf114b48659bddd5f23b294a9531c411da97
Leonard Pauli <leonardpauli@me.com>
2018-10-05 21:56:06 +0200

    app.java.maven: added example-simple + notes

---
+++ app/java/maven/example-simple/src/test/java/com/myorg/mypackage/MainTest.java
@@ -0,0 +1,11 @@
+package com.myorg.mypackage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+	@Test
+	public void testMain() {
+		assertEquals(2, 1+1);
+	}
+}
