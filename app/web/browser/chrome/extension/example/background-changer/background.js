1db70f70b065af0c08667c7da32c7f0e5b27b166
Leonard Pauli <leonardpauli@me.com>
2018-10-17 01:33:04 +0200

    app.web.browser.chrome.extension: added notes + example.background-changer

---
+++ app/web/browser/chrome/extension/example/background-changer/background.js
@@ -0,0 +1,20 @@
+const onInstalled = ()=> {
+	console.log('post-install')
+
+	chrome.storage.sync.set({
+		color: 'hsl(134, 55%, 48%)',
+	}, ()=> {
+		console.log('store updated')
+	})
+
+	const pageRules = [{
+		conditions: [new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: {hostEquals: 'developer.chrome.com'},
+		})],
+		actions: [new chrome.declarativeContent.ShowPageAction()]
+	}]
+
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, ()=>
+		chrome.declarativeContent.onPageChanged.addRules(pageRules))
+}
+chrome.runtime.onInstalled.addListener(onInstalled)
