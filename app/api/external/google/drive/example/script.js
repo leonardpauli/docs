45070cfc88fa57752e85e49df6a6babb0939f3e0
Leonard Pauli <leonardpauli@me.com>
2019-01-12 16:41:19 +0100

    app.api.external.google.drive: quick start example working

---
+++ app/api/external/google/drive/example/script.js
@@ -0,0 +1,125 @@
+// created by Leonard Pauli, 12 jan 2019
+// based on https://developers.google.com/drive/api/v3/quickstart/js
+
+
+const config = {
+	api: {
+		google: {
+			web: {},
+			load: ()=> fetch('/files/client_id.json')
+				.then(a=> a.json())
+				.then(a=> Object.assign(config.api.google.web, a.web)),
+		},
+	},
+	load: ()=> fetch('/files/client_config.json')
+		.then(a=> a.json())
+		.then(a=> {
+			config.api.google.web.key = a.api.google.web.key
+		}),
+}
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+	(async ()=> {
+		await config.load()
+		await config.api.google.load()
+		gapi.load('client:auth2', initClient)	
+	})().catch(console.error)
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+	gapi.client.init({
+		apiKey: config.api.google.web.key,
+		clientId: config.api.google.web.client_id,
+		discoveryDocs: DISCOVERY_DOCS,
+		scope: SCOPES
+	}).then(function () {
+		// Listen for sign-in state changes.
+		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+		// Handle the initial sign-in state.
+		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+		authorizeButton.onclick = handleAuthClick;
+		signoutButton.onclick = handleSignoutClick;
+	}, function(error) {
+		appendPre(JSON.stringify(error, null, 2));
+	});
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+		authorizeButton.style.display = 'none';
+		signoutButton.style.display = 'block';
+		listFiles();
+	} else {
+		authorizeButton.style.display = 'block';
+		signoutButton.style.display = 'none';
+	}
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+	gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+	var pre = document.getElementById('content');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+}
+
+/**
+ * Print files.
+ */
+function listFiles() {
+	gapi.client.drive.files.list({
+		'pageSize': 10,
+		'fields': "nextPageToken, files(id, name)"
+	}).then(function(response) {
+		appendPre('Files:');
+		var files = response.result.files;
+		if (files && files.length > 0) {
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				appendPre(file.name + ' (' + file.id + ')');
+			}
+		} else {
+			appendPre('No files found.');
+		}
+	});
+}
